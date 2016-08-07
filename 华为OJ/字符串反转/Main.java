84b04726cdd98c874e884ae15444c740c67d9f89
e <sgc0515@gmail.com>
2016-08-07 17:07:06 +0800

    更新字符串反转

--- 华为OJ/字符串反转/Main.java
+++ 华为OJ/字符串反转/Main.java
@@ -4,47 +4,17 @@
  * @since 2016.03.29
  * @explain 实现"I am a student"反转成"student a am I"
  */
-import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        int i = 0, j = 0;
-        char temp;
-        char[] str;
-        // 使用 BufferedReader 在控制台读取字符
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        StringBuilder sb = new StringBuilder(line);
-        // 先进行全盘反转
-        j = sb.length() - 1;
-        str = sb.toString().toCharArray();
-        while (i < j) {
-            temp = str[i];
-            str[i] = str[j];
-            str[j] = temp;
-            i++;
-            j--;
-        }
-        System.out.println(str);
-        // 再进行部分反转，如果不是空格则开始反转单词
-        i = 0;
-        while (i < sb.length() - 1) {
-            int begin = 0, end = 0;
-            if (str[i] != ' ') {
-                begin = i;
-                while (i < sb.length() && str[i] != ' ') {
-                    end = i;
-                    i++;
-                }
-            }
-            while (begin < end) {
-                temp = str[begin];
-                str[begin] = str[end];
-                str[end] = temp;
-                begin++;
-                end--;
-            }
-            i++;
-        }
-        System.out.println(str);
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	String line = sc.nextLine();
+    	String[] str = line.split(" ");
+    	StringBuffer sb = new StringBuffer();
+     	for(int i = str.length - 1; i >= 0 ; i--){
+    		sb.append(str[i] + " ");
+    	}
+     	System.out.println(sb.substring(0, sb.length() - 1));
     }
 }
