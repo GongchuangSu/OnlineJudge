41db9fb103186005529e55a12b557909ce4218a9
e <sgc0515@gmail.com>
2016-08-19 20:36:13 +0800

    更新名字漂亮度

--- 华为OJ/名字的漂亮度/Main.java
+++ 华为OJ/名字的漂亮度/Main.java
@@ -1,14 +1,24 @@
 /**
- * @description 名字的复杂度
+ * @description 名字的漂亮度
  * @author GongchuangSu
- * @since 2016.03.29
+ * @since 2016.08.19
  * @explain 给出一个名字，该名字有26个字符串组成，定义这个字符串的“漂亮度”是其所有字母“漂亮度”的总和。
- *          每个字母都有一个“漂亮度”，范围在1到26之间。没有任何两个字母拥有相同的“漂亮度”。字母忽略大小写。
- *          给出多个名字，计算每个名字最大可能的“漂亮度”。
- *          名字的漂亮度=26*字母个数最多的+25*字母个数其次的+24*字母个数再其次的……
+ *          每个字母都有一个“漂亮度”，范围在1到26之间。没有任何两个字母拥有相同的“漂亮度”。字母忽略大
+ *          小写。给出多个名字，计算每个名字最大可能的“漂亮度”。
+ *          输入：整数N，后续N个名字
+ *          输出：每个名称可能的最大漂亮程度
+ *          样例：
+ *             输入：2
+ *                  zhangsan
+ *                  lisi
+ *             输出：192
+ *                   101
+ * @知识点 字符串
  */
+
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
@@ -32,8 +42,9 @@ public class Main {
                     if (arr[k] == 0)
                         break;
                 }
-                System.out.print(count);
+                System.out.println(count);
             }
         }
+        scanner.close();
     }
 }
