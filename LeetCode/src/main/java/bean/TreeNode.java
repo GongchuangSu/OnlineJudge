1403113921719fcc3ad55259f1a19a10d2fdf530
GongchuangSu <sgc0515@gmail.com>
2020-04-04 17:58:42 +0800

    [A]112.path sum

---
+++ LeetCode/src/main/java/bean/TreeNode.java
@@ -0,0 +1,22 @@
+package bean;
+
+/**
+ * 树节点定义
+ * @author GongchuangSu
+ * @Description
+ * @Example
+ * @date 2020.04.04
+ */
+public class TreeNode {
+	
+	public int val;
+	
+	public TreeNode left;
+	
+	public TreeNode right;
+	
+	public TreeNode(int x){
+		val = x;
+	}
+	
+}
---
+++ LeetCode/src/test/java/solutions/_112_path_sum/CommonSolutionTest.java
@@ -0,0 +1,12 @@
+package solutions._112_path_sum;
+
+import org.junit.Test;
+
+
+public class CommonSolutionTest {
+	
+	@Test
+	public void hasPathSum() {
+		// TODO
+	}
+}
