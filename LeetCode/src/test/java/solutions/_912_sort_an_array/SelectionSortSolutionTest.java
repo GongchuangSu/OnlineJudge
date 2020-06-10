af0e470bd2e993c9fcd707559391a00e1916979f
GongchuangSu <sgc0515@gmail.com>
2020-06-11 07:27:15 +0800

    912.sort an array implement by select sort

--- LeetCode/src/test/java/solutions/_912_sort_an_array/SelectionSortSolutionTest.java
+++ LeetCode/src/test/java/solutions/_912_sort_an_array/SelectionSortSolutionTest.java
@@ -1,5 +1,15 @@
-import static org.junit.Assert.*;
+package solutions._912_sort_an_array;
+
+import org.junit.Test;
+
+import java.util.Arrays;
 
 public class SelectionSortSolutionTest {
 	
+	@Test
+	public void sortArray () {
+		SelectionSortSolution solution = new SelectionSortSolution();
+		System.out.println(Arrays.toString(solution.sortArray(new int[]{5,2,3,1})));
+		System.out.println(Arrays.toString(solution.sortArray(new int[]{5,1,1,2,0,0})));
+	}
 }
